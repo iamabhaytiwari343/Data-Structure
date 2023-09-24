08647469da7c4e4f7984ddbf22663919a934502f
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-09-24 23:15:04 +0530

    linkedlist

---
+++ linkedlist/ll1.java
@@ -0,0 +1,16 @@
+class Node{
+    int data;
+    Node next;
+    public Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+public class ll1 {
+   Node head;
+   public ll1(){
+    this.head=null;
+   } 
+   
+
+}
